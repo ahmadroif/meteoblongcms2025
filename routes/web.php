36e5c6639971b5c257fe8f2b4582122a3c3f5894
Ramires Aries <satriaahmadroif@gmail.com>
2025-04-13 01:45:43 +0700

    Menambahkan model, migration, dan controller untuk Product, CustomerRequest, RestockOrder

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/articles', [ArticleController::class, 'index']);
+Route::get('/articles/{id}', [ArticleController::class, 'show']);
+
